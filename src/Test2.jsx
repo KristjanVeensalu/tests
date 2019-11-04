0a4c791823ea81a337fffe5715a51da27c2fe9a3
Guren <Kristjan.Veensalu@gmail.com>
2019-11-04 13:21:38 +0200

    Test2

--- src/Test2.jsx
+++ src/Test2.jsx
@@ -1,25 +1,45 @@
 import React from "react";
 import {TiHeartFullOutline} from "react-icons/ti";
 
-const Test2 = () => {
-  return (
-    <div>
-      <div className={"description"}>
-        Ülesanne 2:
-        <p>
-          Rakendus peab arvet pidama mitu korda on südame ikooni klikitud ning vastava numbri <code>likes:</code> järel kuvama.
-        </p>
-      </div>
 
-      <div style={{display:"flex", alignItems: "center"}}>
-        <div>
-          Likes: [replace me]
+class Test2 extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={clicks:0};
+  }
+
+
+  handleClick = () => {
+    this.setState((prevState) => ({
+      clicks:prevState.clicks+1
+    }));
+
+  }
+  
+
+render() {
+  return (
+      <div>
+        <div className={"description"}>
+          Ülesanne 2:
+          <p>
+            Rakendus peab arvet pidama mitu korda on südame ikooni klikitud ning vastava numbri <code>likes:</code> järel kuvama.
+          </p>
         </div>
-        <TiHeartFullOutline />
-        <div>Click me</div>
+
+        <div style={{display:"flex", alignItems: "center"}}>
+          <div>
+            Likes: {this.state.clicks}
+          </div>
+          <TiHeartFullOutline onClick={this.handleClick.bind(this)}/>
+          <div>Click me</div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+
 
 export default Test2;
