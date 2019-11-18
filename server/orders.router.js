4d643ceb1ea496188816559c7d6f0a566a57c8f9
Guren <Kristjan.Veensalu@gmail.com>
2019-11-18 12:53:43 +0200

    test5, 6

--- server/orders.router.js
+++ server/orders.router.js
@@ -3,10 +3,28 @@ const router = express.Router();
 const {Order} = require("./order.model.js");
 const mongoose = require("mongoose");
 
-router.get("/", async (req, res)=>{
-  const xs = await Order.find({});
-  res.send(xs);
+
+router.post("/", (req,res) =>{
+
+	const item1 = new Item(props);
+	item1.save( err => {
+		if(err){
+			console.log("Error", err);
+			res.send(500);
+			return;
+		}
+		console.log("Success create");
+		res.send(201);
+	});
 });
+
+
+
+
+//router.get("/", async (req, res)=>{
+//  const xs = await Order.find({});
+//  res.send(xs);
+//});
 
 module.exports = router;
 
--- src/Test6.jsx
+++ src/Test6.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import PropTypes from "prop-types";
 // import {toast} from "react-toastify";
 
 class Test6 extends React.PureComponent {
+
+
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+  };
+
+
+  constructor(props){
+    super(props);
+    this.state={
+      fullName: "",
+      burger: "",
+      drink:"",
+    };
+  }
+
+  handleSubmit=(event)=>{
+    event.preventDefault();
+    fetch("/", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+    })
+    .then(res =>res.json())
+    .then(data =>{
+      console.log("data", data);
+      this.props.history.push("/");
+    })
+    .catch(err =>{
+      console.log(err);
+      
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
   render() {
     return (
       <>
@@ -26,15 +65,15 @@ class Test6 extends React.PureComponent {
 
         </div>
         <div className="ds">
-          <form className="ds-item style-2">
+          <form className="ds-item style-2" onSubmit={this.handleSubmit}>
             <h3 className="style-2">Andmebaasi päring</h3>
             <div className={"row"}>
               <label htmlFor="fullName">Kliendi nimi</label>
-              <input name="fullName" type="text" />
+              <input name={"fullName"} type="text" onChange = {this.handleChange} />
             </div>
             <div className={"row"}>
               <label htmlFor="burger">Burger</label>
-              <select name="burger">
+              <select name={"burger"} onChange = {this.handleChange}>
                 <option value="">-</option>
                 <option value="megaBurger">Megaburger</option>
                 <option value="baconBurger">Peekoniburger</option>
@@ -43,7 +82,7 @@ class Test6 extends React.PureComponent {
             </div>
             <div className={"row"}>
               <label htmlFor="drink">Jook</label>
-              <select name="drink">
+              <select name={"drink"} onChange = {this.handleChange}>
                 <option value="">-</option>
                 <option value="coke">Coca-Cola</option>
                 <option value="sprite">Sprite</option>
